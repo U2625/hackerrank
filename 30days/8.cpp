a2ec1e5052dae30e4f9d3089cae753212a288471
U2625 <nastassii@gmail.com>
2019-08-16 19:12:06 +0300

    Dictionaries and Maps

---
+++ 30days/8.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+   
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    map<string, int> phonebook;
+    std::map<char,int>::iterator it;
+
+    string name;
+    int num;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> name;
+        cin >> num;
+        phonebook.insert (pair<string, int>(name, num));
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> name;
+        if (phonebook.find (name) == phonebook.end())
+        {
+            cout << "Not found" << endl;
+        } 
+        else 
+        {
+            cout << name << "=" << phonebook.find(name)->second << endl;
+        }
+    }
+}
